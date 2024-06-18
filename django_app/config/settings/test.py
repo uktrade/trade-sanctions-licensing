5d9329ba61f95b0d70cd7eea3583642ccfc6739a
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-06-18 13:42:25 +0100

    added custom uploader and auth

--- django_app/config/settings/test.py
+++ django_app/config/settings/test.py
@@ -9,7 +9,4 @@ BASE_FRONTEND_TESTING_URL = "http://apply-for-a-licence:8000"
 ENVIRONMENT = "test"
 
 # we don't want to connect to ClamAV in testing, redefine and remove from list
-FILE_UPLOAD_HANDLERS = (
-    "core.custom_upload_handler.CustomFileUploadHandler",
-    "django.core.files.uploadhandler.TemporaryFileUploadHandler",
-)  # Order is important
+FILE_UPLOAD_HANDLERS = ("core.custom_upload_handler.CustomFileUploadHandler",)  # Order is important
