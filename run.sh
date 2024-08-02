6042cad71d7f8148d75ce5db964623a1fe4a3611
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-08-02 16:32:18 +0100

    Adding procfile

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 
-python django_app/manage.py flush --no-input || true
+echo "Running migrations"
+python manage.py migrate
 python django_app/manage.py collectstatic --no-input
 
 # Start webserver
-if [ -n "${COPILOT_ENVIRONMENT_NAME}" ]; then
-    echo "Running in DBT Platform"
-    opentelemetry-instrument gunicorn django_app.config.wsgi --config django_app/config/gunicorn.py
-else
-    echo "Running in Cloud Foundry"
-    gunicorn django_app.config.wsgi --bind 0.0.0.0:8080 --capture-output --config django_app/config/gunicorn.py
-fi
+echo "Running in DBT Platform"
+opentelemetry-instrument gunicorn django_app.config.wsgi --config django_app/config/gunicorn.py
