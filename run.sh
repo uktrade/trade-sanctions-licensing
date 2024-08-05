a07e51e5c2862ea59efbed1fb1eb7a5faeea9b25
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-08-05 18:47:48 +0100

    updating Procfile

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 echo "Running migrations"
-python manage.py migrate
+python django_app/manage.py migrate
 python django_app/manage.py collectstatic --no-input
 
 # Start webserver
 echo "Running in DBT Platform"
-opentelemetry-instrument gunicorn django_app.config.wsgi --config django_app/config/gunicorn.py
+gunicorn django_app.config.wsgi --config django_app/config/gunicorn.py
