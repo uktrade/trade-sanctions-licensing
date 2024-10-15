caf80ab9fc624c791e5ae94aa8d8fd5b1c7f95e9
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-10-15 09:48:30 +0100

    remove checks from js

--- django_app/apply_for_a_licence/static/apply_for_a_licence/javascript/conditional_aria_controls.js
+++ django_app/apply_for_a_licence/static/apply_for_a_licence/javascript/conditional_aria_controls.js
@@ -2,14 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const yesChoice = document.querySelector('input[value="yes"]');
     const noChoice = document.querySelector('input[value="no"]');
 
-    noChoice.addEventListener("change", function () {
-        if (this.checked) {
-            this.removeAttribute("aria-controls");
-        }
-    });
-    yesChoice.addEventListener("change", function () {
-        if (this.checked) {
-            yesChoice.setAttribute("aria-controls", conditionalChoiceId);
-        }
-    });
+    noChoice.removeAttribute("aria-controls");
+    yesChoice.setAttribute("aria-controls", conditionalChoiceId);
 });
