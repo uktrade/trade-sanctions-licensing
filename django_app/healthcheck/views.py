a219c84421d9faa9031d21b83c9cc9a22634da09
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-11-07 10:31:24 +0000

    DST-548 - Fix the DBT Platform health checks for RAB and Licensing

--- django_app/healthcheck/views.py
+++ django_app/healthcheck/views.py
@@ -20,8 +20,8 @@ class HealthCheckView(View):
 
         start = time.time()
         is_db_good = db_check()
-        #  is_s3_good = s3_check()
-        all_good = is_db_good and True
+        is_s3_good = s3_check()
+        all_good = is_db_good and is_s3_good
 
         end = time.time()
         time_taken = round(end - start, 3)
