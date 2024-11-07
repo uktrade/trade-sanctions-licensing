5c271a54441d2d64569ec6daa6d81996dbb114b4
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-11-07 10:28:20 +0000

    update run.sh to install wkhtmltopdf

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -e
 
+# install wkhtmltopdf and it's dependencies
+echo "Installing wkhtmltopdf"
+apt-get update && apt-get install -y \
+  wget \
+  xfonts-75dpi \
+  fontconfig \
+  libxrender1 \
+  libxexr6 \
+  ca-certificates
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+dpkg -i ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+apt install -y ./wkhtmltox_0.12.6-1.bionic_amd64.deb
+
 echo "Running migrations"
 python django_app/manage.py migrate
 python django_app/manage.py collectstatic --no-input
