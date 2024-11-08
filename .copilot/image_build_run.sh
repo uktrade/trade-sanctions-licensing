03d5111d96d99f7174f6103352262a48031e09c2
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-11-08 11:13:36 +0000

    update image

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -11,20 +11,25 @@ export DATABASE_CREDENTIALS='{"username": "postgres", "password": "password", "e
 export DJANGO_SETTINGS_MODULE='config.settings.deploy.development'
 
 echo "Installing wkhtmltopdf"
-sudo apt-get update && apt-get install -y \
-  wget \
-  xfonts-75dpi \
-  fontconfig \
-  libxrender1 \
-  libxexr6 \
-  ca-certificates
+yum install -y \
+  fontconfig
+  freetype
+  freetype=deve;
+  libX11
+  libXext
+  libextrender
+  xorg-x11-fonts-Type1
+  xorg-x11-fonts-75dpi
+  wget
 
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-sudo apt install ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+#wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+#sudo apt install ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+#rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
 
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-sudo apt install ./wkhtmltox_0.12.6-1.bionic_amd64.deb
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
+#wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6/wkhtmltox_0.12.6-1.centos7.x86_64.rpm
+yum localinstall -y wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
+rm wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
 
 echo "Running django_app/manage.py collectstatic --noinput"
 python django_app/manage.py collectstatic --no-input
