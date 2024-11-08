0f015edb0b3776836fd015b67d66c526af05313e
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2024-11-08 16:07:07 +0000

    trying config change

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -10,26 +10,26 @@ export DEBUG='True'
 export DATABASE_CREDENTIALS='{"username": "postgres", "password": "password", "engine": "postgres", "port": 5432, "dbname": "postgres", "host": "db", "dbInstanceIdentifier": "emt-db"}'
 export DJANGO_SETTINGS_MODULE='config.settings.deploy.development'
 
-echo "Installing wkhtmltopdf"
-yum install -y \
-  fontconfig \
-  freetype \
-  freetype-devel \
-  libX11 \
-  libXext \
-  libextrender \
-  xorg-x11-fonts-Type1 \
-  xorg-x11-fonts-75dpi \
-  wget
-
-#wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-#sudo apt install ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-#rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
-#wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6/wkhtmltox_0.12.6-1.centos7.x86_64.rpm
-yum localinstall -y wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
-rm wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
+#echo "Installing wkhtmltopdf"
+#yum install -y \
+#  fontconfig \
+#  freetype \
+#  freetype-devel \
+#  libX11 \
+#  libXext \
+#  libextrender \
+#  xorg-x11-fonts-Type1 \
+#  xorg-x11-fonts-75dpi \
+#  wget
+#
+##wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+##sudo apt install ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+##rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+#
+#wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
+##wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6/wkhtmltox_0.12.6-1.centos7.x86_64.rpm
+#yum localinstall -y wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
+#rm wkhtmltox-0.12.6-1.amazonlinux2.x86_64.rpm
 
 echo "Running django_app/manage.py collectstatic --noinput"
 python django_app/manage.py collectstatic --no-input
