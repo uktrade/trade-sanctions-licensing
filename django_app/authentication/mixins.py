a02793f5419981099790ec370a9d12f1938de4d6
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-12-04 11:49:55 +0000

    Disabling one-login temporairly

--- django_app/authentication/mixins.py
+++ django_app/authentication/mixins.py
@@ -1,9 +1,24 @@
 from core.sites import is_apply_for_a_licence_site, is_view_a_licence_site
 from django.contrib.auth.mixins import LoginRequiredMixin as DjangoLoginRequiredMixin
+from django.contrib.auth.models import AnonymousUser
+from django.http import HttpRequest
 from django.urls import reverse
 
 
+class AuthenticatedAnonymousUser(AnonymousUser):
+    @property
+    def is_authenticated(self):
+        return True
+
+
 class LoginRequiredMixin(DjangoLoginRequiredMixin):
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        if is_apply_for_a_licence_site(request.site):
+            # we're currently not enforcing one-login for the apply site
+            request.user = AuthenticatedAnonymousUser()
+        return super().dispatch(request, *args, **kwargs)
+
     def get_login_url(self):
         if is_apply_for_a_licence_site(self.request.site):
             # it's the public site, use GOV.UK One Login
