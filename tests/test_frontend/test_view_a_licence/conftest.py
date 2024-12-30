1107011e05e4c40e77d01f775cd22a2138507bbb
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2024-12-30 10:35:12 +0000

    comments

--- tests/test_frontend/test_view_a_licence/conftest.py
+++ tests/test_frontend/test_view_a_licence/conftest.py
@@ -12,6 +12,11 @@ class PlaywrightTestBase(BasePlaywrightTestBase):
 
 @pytest.fixture(autouse=True, scope="function")
 def bypass_login(monkeypatch, staff_user):
+    """Overrides the get_user function to always return the staff user without authentication.
+
+    Effectively bypasses the login process for all frontend tests.
+    """
+
     def patched_get_user(request):
         return staff_user
 
