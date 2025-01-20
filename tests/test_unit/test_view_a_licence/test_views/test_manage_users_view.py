9e2dd7aba49817e9ecc4bca511247ed105c9ee0a
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2025-01-20 18:28:13 +0000

    DST-947 - Adding public users to manage users portal

--- tests/test_unit/test_view_a_licence/test_views/test_manage_users_view.py
+++ tests/test_unit/test_view_a_licence/test_views/test_manage_users_view.py
@@ -1,18 +1,28 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-from tests.factories import UserFactory
+from tests.factories import LicenceFactory, UserFactory
 
 
 class TestManageUsersView:
     def test_get_context_data(self, vl_client_logged_in, staff_user):
-        inactive_user = UserFactory.create(is_active=False, is_staff=False)
+        inactive_staff_user = UserFactory.create(is_active=False, is_staff=False)
+        public_user = UserFactory.create(username="urn:fdc:gov.uk:public_user", is_active=True)
+
+        LicenceFactory.create_batch(2, user=public_user, status="submitted")
+        LicenceFactory.create_batch(4, user=public_user, status="draft")
 
         response = vl_client_logged_in.get(reverse("view_a_licence:manage_users"))
-        assert response.context["pending_users"].count() == 1
-        assert response.context["pending_users"].first() == inactive_user
-        assert response.context["accepted_users"].count() == 1
-        assert response.context["accepted_users"].first() == staff_user
+        assert response.context["pending_staff_users"].count() == 1
+        assert response.context["pending_staff_users"].first() == inactive_staff_user
+        assert response.context["accepted_staff_users"].count() == 1
+        assert response.context["accepted_staff_users"].first() == staff_user
+        assert response.context["public_users"].count() == 1
+
+        returned_public_user = response.context["public_users"].first()
+        assert returned_public_user == public_user
+        assert returned_public_user.submitted_applications_count == 2
+        assert returned_public_user.draft_applications_count == 4
 
     def test_make_active(self, vl_client_logged_in):
         inactive_user = UserFactory.create(is_active=False, is_staff=False)
