abf131e3ade2ffe0bf1e9898975969ec26e1cd5c
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2025-01-20 15:33:06 +0000

    Enabling admin panel on lower environments to help with debugging

--- tests/test_unit/test_core/test_urls.py
+++ tests/test_unit/test_core/test_urls.py
@@ -39,3 +39,27 @@ def test_private_urls_true(settings, al_client):
     # assert view urls return 403 forbidden
     response = al_client.get("/view/")
     assert response.status_code == 403
+
+
+@override_settings(INCLUDE_PRIVATE_URLS=False, DEBUG=False)
+def test_admin_panel_hidden_normal(settings, al_client):
+    reload_urlconf()
+    assert not settings.INCLUDE_PRIVATE_URLS
+    response = al_client.get("/admin/")
+    assert response.status_code == 404
+
+
+@override_settings(INCLUDE_PRIVATE_URLS=True, DEBUG=False)
+def test_admin_panel_hidden_debug_false(settings, al_client):
+    reload_urlconf()
+    assert settings.INCLUDE_PRIVATE_URLS
+    response = al_client.get("/admin/")
+    assert response.status_code == 404
+
+
+@override_settings(INCLUDE_PRIVATE_URLS=True, DEBUG=True)
+def test_admin_panel_visible(settings, al_client):
+    reload_urlconf()
+    assert settings.INCLUDE_PRIVATE_URLS
+    response = al_client.get("/admin/")
+    assert response.status_code == 302
