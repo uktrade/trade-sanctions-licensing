7ca49c3a67e99bcb7c1ba99cb3771aebd13c48aa
Morgan Rees <morganrees@DBT000491.local>
2025-02-07 15:38:48 +0000

    Management command for applications tasks

---
+++ django_app/core/management/commands/manage_applications.py
@@ -0,0 +1,43 @@
+import datetime
+
+from apply_for_a_licence.models import Licence
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.urls import reverse
+from django.utils import timezone
+from utils.notifier import send_email
+
+
+class Command(BaseCommand):
+    help = (
+        "Deletes licence applications older than DRAFT_APPLICATION_EXPIRY_DAYS number of days"
+        "Sends an email to applications a week before DRAFT_APPLICATION_EXPIRY_DAYS"
+        "Usage: pipenv run django_app/python manage.py delete_licence_applications"
+    )
+
+    def handle(self, *args, **options):
+        delete_threshold = timezone.now() - datetime.timedelta(days=settings.DRAFT_APPLICATION_EXPIRY_DAYS)
+        # Delete applications older than DRAFT_APPLICATION_EXPIRY_DAYS
+        stale_licences = Licence.objects.filter(created_at__lt=delete_threshold)
+        for licence_object in stale_licences:
+            licence_object.delete()
+            self.stdout.write(self.style.SUCCESS(f"Successfully deleted Licence application {licence_object.id}"))
+
+        # Email applicants whose licenses are 7 days from DRAFT_APPLICATION_EXPIRY_DAYS
+        email_threshold = delete_threshold - datetime.timedelta(days=7)
+        email_licences = Licence.objects.filter(created_at__date=email_threshold.date())
+        for licence_object in email_licences:
+            send_email(
+                email=licence_object.applicant_user_email_address,
+                template_id=settings.DELETE_lICENCE_APPLICATION_TEMPLATE_ID,
+                context={
+                    "name": licence_object.applicant_full_name,
+                    "application_number": licence_object.reference,
+                    "url": reverse("dashboard"),
+                },
+            )
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully send email to {licence_object.email} for Licence application {licence_object.id}"
+                )
+            )
