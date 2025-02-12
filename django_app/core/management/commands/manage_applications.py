26e7461190af783b09d0993d69efdde18778192e
Morgan Rees <morganrees@DBT000491.local>
2025-02-12 14:43:13 +0000

    updates to manage application

--- django_app/core/management/commands/manage_applications.py
+++ django_app/core/management/commands/manage_applications.py
@@ -1,5 +1,6 @@
 import datetime
 
+from apply_for_a_licence.choices import StatusChoices
 from apply_for_a_licence.models import Licence
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -18,18 +19,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         delete_threshold = timezone.now() - datetime.timedelta(days=settings.DRAFT_APPLICATION_EXPIRY_DAYS)
         # Delete applications older than DRAFT_APPLICATION_EXPIRY_DAYS
-        stale_licences = Licence.objects.filter(created_at__lt=delete_threshold)
+        stale_licences = Licence.objects.filter(created_at__lt=delete_threshold, status=StatusChoices.draft)
         for licence_object in stale_licences:
+            licence_id = licence_object.id
             licence_object.delete()
-            self.stdout.write(self.style.SUCCESS(f"Successfully deleted Licence application {licence_object.id}"))
+            self.stdout.write(self.style.SUCCESS(f"Successfully deleted Licence application {licence_id}"))
 
         # Email applicants whose licenses are 7 days from DRAFT_APPLICATION_EXPIRY_DAYS
-        email_threshold = delete_threshold - datetime.timedelta(days=7)
-        email_licences = Licence.objects.filter(created_at__date=email_threshold.date())
+        email_threshold = delete_threshold + datetime.timedelta(days=7)
+        email_licences = Licence.objects.filter(created_at__date=email_threshold.date(), status=StatusChoices.draft)
+
         for licence_object in email_licences:
             send_email(
                 email=licence_object.applicant_user_email_address,
-                template_id=settings.DELETE_lICENCE_APPLICATION_TEMPLATE_ID,
+                template_id=settings.DELETE_LICENCE_APPLICATION_TEMPLATE_ID,
                 context={
                     "name": licence_object.applicant_full_name,
                     "application_number": licence_object.reference,
