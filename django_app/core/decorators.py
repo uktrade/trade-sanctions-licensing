1544cffe02fd19eb851bed4efa69bd98ae1cc0e5
Christopher Pettinga <chris.pettinga@digital.trade.gov.uk>
2025-02-19 09:41:52 +0000

    fixing tests

--- django_app/core/decorators.py
+++ django_app/core/decorators.py
@@ -5,8 +5,8 @@ def reset_last_activity_session_timestamp(view_func):
     """Decorator to reset the last activity session timestamp, refresh it."""
 
     def wrapper(request, *args, **kwargs):
-        response = view_func(request, *args, **kwargs)
         update_last_activity_session_timestamp(request)
+        response = view_func(request, *args, **kwargs)
         return response
 
     return wrapper
