0c3debdc52ace6c33eca0e7fb08783ef65809591
Morgan Rees <65167892+morganmaerees@users.noreply.github.com>
2025-03-05 09:52:56 +0000

    update admin command (#210)
    Co-authored-by: Morgan Rees <morganrees@DBT000491.local>

--- django_app/view_a_licence/management/commands/make_admin_user.py
+++ django_app/view_a_licence/management/commands/make_admin_user.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import Group, User
 from django.core.management.base import BaseCommand, CommandParser
 
 
@@ -22,9 +23,11 @@ class Command(BaseCommand):
         user_objects = User.objects.all()
         for email in emails:
             try:
+                internal_user_group = Group.objects.get(name=settings.INTERNAL_USER_GROUP_NAME)
                 existing_user = user_objects.get(email=email)
                 existing_user.is_staff = True
                 existing_user.is_active = True
+                existing_user.groups.add(internal_user_group)
                 existing_user.save()
                 self.stdout.write(self.style.SUCCESS("User updated successfully"))
 
