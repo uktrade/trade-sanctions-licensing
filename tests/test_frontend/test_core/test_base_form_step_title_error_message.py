9f78a6c29156408e72bde4159669f087c0586991
Abdallah Harun <abdallah.harun@digital.trade.gov.uk>
2025-04-16 13:06:09 +0100

    Dst 1034 (#246)
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear
    
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear
    
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear
    
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear
    
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear
    
    * DST-1034 - Adding 'Error: ' to title browser tab text when errors appear

---
+++ tests/test_frontend/test_core/test_base_form_step_title_error_message.py
@@ -0,0 +1,13 @@
+import re
+
+from playwright.sync_api import expect
+
+from tests.test_frontend.conftest import PlaywrightTestBase
+
+
+class TestBaseFormStepTitleErrorMessage(PlaywrightTestBase):
+    def test_error_text_in_title(self):
+        self.start_new_application()
+        self.page.get_by_role("button", name="Save and continue").click()
+
+        expect(self.page).to_have_title(re.compile("Error: "))
