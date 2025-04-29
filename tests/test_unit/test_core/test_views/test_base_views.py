a97b6c2439285349f988d9645413cb9d1b058e53
Morgan Rees <morganrees@DBT000491.local>
2025-04-29 17:28:18 +0100

    fix unit tests

--- tests/test_unit/test_core/test_views/test_base_views.py
+++ tests/test_unit/test_core/test_views/test_base_views.py
@@ -6,12 +6,8 @@ class TestBaseLicenceFormView:
     def test_incorrect_user_get_form_kwargs(self, authenticated_al_client):
         licence = Licence.objects.create(user=None)
 
-        session = authenticated_al_client.session
-        session["licence_id"] = licence.id
-        session.save()
-
         response = authenticated_al_client.get(
-            reverse("your_details"),
+            reverse("your_details", kwargs={"licence_pk": licence.id}),
         )
 
         assert response.status_code == 404
@@ -19,12 +15,8 @@ class TestBaseLicenceFormView:
     def test_correct_user_get_form_kwargs(self, authenticated_al_client, test_apply_user):
         licence = Licence.objects.create(user=test_apply_user)
 
-        session = authenticated_al_client.session
-        session["licence_id"] = licence.id
-        session.save()
-
         response = authenticated_al_client.get(
-            reverse("your_details"),
+            reverse("your_details", kwargs={"licence_pk": licence.id}),
         )
 
         assert response.status_code == 200
