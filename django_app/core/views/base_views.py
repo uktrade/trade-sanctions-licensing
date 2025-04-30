0fb8881a8d805e01b9c4e5054acde4bf7a27431a
Morgan Rees <morganrees@DBT000491.local>
2025-04-30 07:54:57 +0100

    redirect if licence_object otherwise go to dashboard

--- django_app/core/views/base_views.py
+++ django_app/core/views/base_views.py
@@ -80,7 +80,10 @@ class BaseSaveAndReturnFormView(BaseSaveAndReturnView, FormView):
 
     def post(self, request: HttpRequest, *args: object, **kwargs: object) -> HttpResponse:
         if self.request.POST.get("skip_link"):
-            return HttpResponseRedirect(reverse("tasklist", kwargs={"licence_pk": self.licence_object.id}))
+            if self.licence_object:
+                return HttpResponseRedirect(reverse("tasklist", kwargs={"licence_pk": self.licence_object.id}))
+            else:
+                return HttpResponseRedirect(reverse("dashboard"))
         else:
             return super().post(request, *args, **kwargs)
 
@@ -105,8 +108,11 @@ class BaseSaveAndReturnFormView(BaseSaveAndReturnView, FormView):
         redirect_to_url = self.request.GET.get("redirect_to_url", None) or self.request.session.pop("redirect_to_url", None)
         if redirect_to_url and url_has_allowed_host_and_scheme(redirect_to_url, allowed_hosts=None):
             if self.redirect_after_post:
-                # we want to redirect the user to a specific page after the form is submitted
-                return reverse(redirect_to_url, kwargs={"licence_pk": self.licence_object.id})
+                if self.licence_object:
+                    # we want to redirect the user to a specific page after the form is submitted
+                    return reverse(redirect_to_url, kwargs={"licence_pk": self.licence_object.id})
+                else:
+                    return reverse("dashboard")
             else:
                 # we don't want to redirect the user just now, but we want to pass the redirect_to URL to the next form,
                 # so it can redirect the user after it is submitted
