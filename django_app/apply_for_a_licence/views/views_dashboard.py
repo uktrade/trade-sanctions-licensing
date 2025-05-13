1e5d91ced867b80e02f776f284734f36247d1438
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-05-13 09:23:11 +0100

    fix redirect

--- django_app/apply_for_a_licence/views/views_dashboard.py
+++ django_app/apply_for_a_licence/views/views_dashboard.py
@@ -15,17 +15,17 @@ class DashboardView(BaseTemplateView):
     def get(self, *args, **kwargs):
         self.applications = self.request.user.licence_applications.order_by("-created_at")
 
-        if self.applications.count() == 0:
-            # if we're here, the user has no applications, so redirect them to start a new one
-            return redirect(reverse("new_application"))
-        else:
-            # let's go through the applications and see if any are over the 28-day limit, if so delete them
-            # we might as well do this here rather than create a complex cron job or management command
-            for application in self.applications:
-                if application.is_expired():
-                    # "pop" the application from the queryset
-                    self.applications = self.applications.exclude(id=application.id)
-                    application.delete()
+        # if self.applications.count() == 0:
+        #     # if we're here, the user has no applications, so redirect them to start a new one
+        #     return redirect(reverse("new_application"))
+        # else:
+        # let's go through the applications and see if any are over the 28-day limit, if so delete them
+        # we might as well do this here rather than create a complex cron job or management command
+        for application in self.applications:
+            if application.is_expired():
+                # "pop" the application from the queryset
+                self.applications = self.applications.exclude(id=application.id)
+                application.delete()
 
         return super().get(*args, **kwargs)
 
