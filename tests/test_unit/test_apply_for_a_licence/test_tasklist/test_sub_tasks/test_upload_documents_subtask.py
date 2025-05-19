5b6c51d18c599c14a05d1057c3186ef44d598a56
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-05-19 12:47:43 +0100

    new test

--- tests/test_unit/test_apply_for_a_licence/test_tasklist/test_sub_tasks/test_upload_documents_subtask.py
+++ tests/test_unit/test_apply_for_a_licence/test_tasklist/test_sub_tasks/test_upload_documents_subtask.py
@@ -15,3 +15,9 @@ class TestPreviousLicensesHeldSubTask:
         assert not sub_task.is_completed
         Document.objects.create(licence=business_licence, temp_file="test123124234.png", original_file_name="test.png")
         assert sub_task.is_completed
+
+    def test_subtask_is_in_progress(self, business_licence):
+        sub_task = UploadDocumentsSubTask(business_licence)
+        assert not sub_task.is_completed
+        business_licence.submitted_documents_form = True
+        assert sub_task.is_in_progress
