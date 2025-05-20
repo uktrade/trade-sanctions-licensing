4941f69cbe8a5f0e49d4fa7b7905111d9d9a33dd
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-05-20 14:08:27 +0100

    debug

--- django_app/authentication/views.py
+++ django_app/authentication/views.py
@@ -108,8 +108,10 @@ class LogoutView(View):
     def get(self, *args: object, **kwargs: object) -> HttpResponse:
         gov_one_logout_url = "https://oidc.integration.account.gov.uk/logout"
         post_logout_redirect_url = self.request.build_absolute_uri(reverse("authentication:session_expired"))
+        print(post_logout_redirect_url)
         if one_login_token := self.request.session.get("_one_login_token", None):
             if oidc_id_token := one_login_token.get("id_token", None):
+                print(oidc_id_token)
                 gov_one_logout_url += f"?id_token_hint={oidc_id_token}&post_logout_redirect_uri={post_logout_redirect_url}"
 
         logout(self.request)
