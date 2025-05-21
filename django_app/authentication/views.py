d9e3abe31657abe6e8bf5fc1c5fcea00841c7d4b
jenriordan <jennifer.riordan@digital.trade.gov.uk>
2025-05-21 08:34:54 +0100

    wip debug

--- django_app/authentication/views.py
+++ django_app/authentication/views.py
@@ -111,11 +111,11 @@ class LogoutView(View):
         if one_login_token := self.request.session.get("_one_login_token", None):
             if oidc_id_token := one_login_token.get("id_token", None):
                 gov_one_logout_url += f"?id_token_hint={oidc_id_token}&post_logout_redirect_uri={post_logout_redirect_url}"
-                logout(self.request)
+                logout(self.request.user)
                 return HttpResponseRedirect(gov_one_logout_url)
 
         # if the token isn't found, forcibly log the user out of our application
-        logout(self.request)
+        logout(self.request.user)
         return HttpResponseRedirect(post_logout_redirect_url)
 
 
